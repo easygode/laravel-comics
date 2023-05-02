a2709c0d6e0f4915e8193297366c00142fbb86c4
easygode <bimalbenvenuti@gmail.com>
2023-05-02 21:19:18 +0200

    routes and header

---
+++ resources/views/layouts/partials/header.blade.php
@@ -0,0 +1,24 @@
+<header>
+    <div class="container">
+        <div class="logo d-flex">
+            <img src="{{ Vite::asset(resources/img/dc-logo.png)}}" alt="" srcset="">
+        </div>
+        <div class="menu">
+            <nav>
+                <ul>
+                    <li><a href="">CHARACTERS</a></li>
+                    <li><a href="">COMICS</a></li>
+                    <li><a href="">MOVIES</a></li>
+                    <li><a href="">TV</a></li>
+                    <li><a href="">GAMES</a></li>
+                    <li><a href="">COLLECTIBLES</a></li>
+                    <li><a href="">VIDEOS</a></li>
+                    <li><a href="">FANS</a></li>
+                    <li><a href="">NEWS</a></li>
+                    <li><a href="">SHOP</a></li>
+                    <li><a href="">Search</a></li>
+                </ul>
+            </nav>
+        </div>
+    </div>
+</header>
