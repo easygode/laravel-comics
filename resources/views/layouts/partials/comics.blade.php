eb43821bbd482fd8b350956127610a5f55e174ab
easygode <bimalbenvenuti@gmail.com>
2023-05-02 21:33:38 +0200

    add pages

---
+++ resources/views/layouts/partials/comics.blade.php
@@ -0,0 +1,53 @@
+{{-- default layout --}}
+@extends('layouts.default')
+
+{{-- page title --}}
+@section('pageTitle', 'Comics');
+
+@section('mainContent')
+<div class="jumbo">
+    <div class="section-title"><h1>current series</h1></div>
+</div>
+<div class="main-container">
+    <div class="card-container">
+
+        @foreach($data as $card)
+        <a class="card-link" href="{{route('comic.details', $loop->index)}}"><div class="cards">
+            <div class="card-image">
+                <img src="{{$card['thumb']}}" alt="">
+            </div>
+            <div>{{$card['series']}}</div>
+        </div> </a>
+        @endforeach
+    
+    </div>
+</div>
+<div class="button-cards">
+    <button> LOAD MORE</button>
+</div>
+
+<section>
+    <div class="container">
+        <div class="info-shop">
+            <img src="/img/buy-comics-digital-comics.png" alt="">
+            <div class="shop-text"> DIGITAL COMICS </div>
+        </div>
+        <div class="info-shop">
+            <img src="/img/buy-comics-merchandise.png" alt="">
+            <div class="shop-text"> DC MERCHANDISE </div>
+        </div>
+        <div class="info-shop">
+            <img src="/img/buy-comics-subscriptions.png" alt="">
+            <div class="shop-text"> SUBSCRIPTION </div>
+        </div>
+        <div class="info-shop">
+            <img src="/img/buy-comics-shop-locator.png" alt="">
+            <div class="shop-text"> COMIC SHOP LOCATOR </div>
+        </div>
+        <div class="info-shop visa">
+            <img src="/img/buy-dc-power-visa.svg" alt="">
+            <div class="shop-text"> DC POWER VISA </div>
+        </div>
+    </div>
+</section>
+@endsection
--- resources/views/layouts/partials/footer.blade.php
+++ resources/views/layouts/partials/footer.blade.php
@@ -1,1 +1,58 @@
-<footer></footer>
+<footer>
+    <div class="footer-container">
+        <div class="footer-nav">
+            <ul>
+                <li><h3>DC COMICS</h3></li>
+                {{-- foreach footer navbar --}}
+                @foreach($navbar as $key => $item)
+                <li> <a href="{{route($key)}}"> {{$item}} </a></li>
+                @endforeach
+                
+                <li><h3>SHOP</h3></li>
+                <li> <a href="#"> Shop DC </a></li>
+                <li> <a href="#"> Shop DC Collectibles </a></li>
+            </ul>
+            <ul>
+                <li><h3>DC</h3></li>
+                <li> <a href="#"> Terms of Use </a></li>
+                <li> <a href="#"> Privacy Policy </a></li>
+                <li> <a href="#"> Ad Choices </a></li>
+                <li> <a href="#"> Advertising </a></li>
+                <li> <a href="#"> Jobs </a></li>
+                <li> <a href="#"> Subscriptions </a></li>
+                <li> <a href="#"> Talent Workshops </a></li>
+                <li> <a href="#"> CPSC Certificates </a></li>
+                <li> <a href="#"> Ratings </a></li>
+                <li> <a href="#"> Shop Help </a></li>
+                <li> <a href="#"> Contact Us </a></li>
+            </ul>
+            <ul>
+                <li><h3>SITES</h3></li>
+                <li> <a href="#"> DC </a></li>
+                <li> <a href="#"> MAD Magazine </a></li>
+                <li> <a href="#"> DC Kid </a></li>
+                <li> <a href="#"> DC Universe </a></li>
+                <li> <a href="#"> DC Power Visa </a></li>
+            </ul>
+        </div>
+        <div class="logo-img">
+            <img src={{"/img/dc-logo-bg.png"}} alt="">
+        </div>
+    </div>
+    
+    <section class="social-bg">
+        <div class="footer-social">
+            <div>
+                <button>SIGN-UP NOW</button>
+            </div>
+            <div class="social-icons">
+                <div>FOLLOW US</div>
+                <img src="/img/footer-facebook.png" alt="">
+                <img src="/img/footer-twitter.png" alt="">
+                <img src="/img/footer-youtube.png" alt="">
+                <img src="/img/footer-pinterest.png" alt="">
+                <img src="/img/footer-periscope.png" alt="">
+            </div> 
+        </div>
+    </section>
+</footer>
